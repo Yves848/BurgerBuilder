6d391311f68b8c51a5e3c838c78e3db65a2a3e9c
yves848 <yves.godart@gmail.com>
2018-08-31 09:47:44 +0200

    Section 8 session 138

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,18 +2,31 @@ import React from 'react';
 
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems'
-import classes from './SideDrawer.css'
+import classes from './SideDrawer.css';
+import Aux from '../../../hoc/_Aux'
+import Backdrop from '../../UI/Backdrop/Backdrop';
 
 
 const sideDrawer = (props) => {
+  let attachedClasses = [classes.SideDrawer, classes.Close];
 
+  if (props.open){
+    attachedClasses = [classes.SideDrawer, classes.Open];
+  }
+
+  console.log('attachedClasses',attachedClasses)
   return (  
-    <div className={classes.SideDrawer}>
-      <Logo></Logo>
+    <Aux>
+    <Backdrop show={props.open} clicked={props.closed}/>   
+    <div className={attachedClasses.join(' ')}>
+      <div className={classes.Logo}>
+        <Logo/>
+      </div>
       <nav>
-        <NavigationItems></NavigationItems>
+        <NavigationItems/>
       </nav>
     </div>
+    </Aux>
   );
 }
 
